c696302faa1c8b52a8d384956f95486b6ab1282c
amit666hol <36861082+amit666hol@users.noreply.github.com>
2020-02-17 16:58:32 +0200

    added _mask

---
+++ pycocotools/_mask.py
@@ -0,0 +1,109 @@
+# encoding: utf-8
+# module pycocotools._mask
+# from C:\Users\Public\python\pytorch\venv\lib\site-packages\pycocotools\_mask.cp37-win_amd64.pyd
+# by generator 1.147
+# no doc
+
+# imports
+import builtins as __builtins__ # <module 'builtins' (built-in)>
+import sys as sys # <module 'sys' (built-in)>
+import numpy as np # C:\Users\Public\python\pytorch\venv\lib\site-packages\numpy\__init__.py
+
+# Variables with simple values
+
+PYTHON_VERSION = 3
+
+__author__ = 'tsungyi'
+
+# functions
+
+def area(*args, **kwargs): # real signature unknown
+    pass
+
+def decode(*args, **kwargs): # real signature unknown
+    pass
+
+def encode(*args, **kwargs): # real signature unknown
+    pass
+
+def frBbox(*args, **kwargs): # real signature unknown
+    pass
+
+def frPoly(*args, **kwargs): # real signature unknown
+    pass
+
+def frPyObjects(*args, **kwargs): # real signature unknown
+    pass
+
+def frUncompressedRLE(*args, **kwargs): # real signature unknown
+    pass
+
+def iou(*args, **kwargs): # real signature unknown
+    pass
+
+def merge(*args, **kwargs): # real signature unknown
+    pass
+
+def toBbox(*args, **kwargs): # real signature unknown
+    pass
+
+def _frString(*args, **kwargs): # real signature unknown
+    pass
+
+def _toString(*args, **kwargs): # real signature unknown
+    pass
+
+# classes
+
+class Masks(object):
+    # no doc
+    def __array__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __init__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    @staticmethod # known case of __new__
+    def __new__(*args, **kwargs): # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
+
+    def __reduce__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __setstate__(self, *args, **kwargs): # real signature unknown
+        pass
+
+
+class RLEs(object):
+    # no doc
+    def __getattribute__(self, *args, **kwargs): # real signature unknown
+        """ Return getattr(self, name). """
+        pass
+
+    def __getattr__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __init__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    @staticmethod # known case of __new__
+    def __new__(*args, **kwargs): # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
+
+    def __reduce__(self, *args, **kwargs): # real signature unknown
+        pass
+
+    def __setstate__(self, *args, **kwargs): # real signature unknown
+        pass
+
+
+# variables with complex values
+
+__loader__ = None # (!) real value is '<_frozen_importlib_external.ExtensionFileLoader object at 0x0000027769AAF408>'
+
+__spec__ = None # (!) real value is "ModuleSpec(name='pycocotools._mask', loader=<_frozen_importlib_external.ExtensionFileLoader object at 0x0000027769AAF408>, origin='C:\\\\Users\\\\Public\\\\python\\\\pytorch\\\\venv\\\\lib\\\\site-packages\\\\pycocotools\\\\_mask.cp37-win_amd64.pyd')"
+
+__test__ = {}
+
